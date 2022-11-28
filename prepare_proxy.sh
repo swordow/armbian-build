ff57440374ca7850248d9e838acc5856d848c4bb
swordow <swordow89@gmail.com>
2022-11-28 07:46:04 -0800

    mod: proxy script more clear

--- prepare_proxy.sh
+++ prepare_proxy.sh
@@ -5,12 +5,12 @@
 ## uncomment the line leading with one '#' and changing "example.com" to proxy domain or ip as well as the port
 
 ## set http and https proxy here for curl 
-# export http_proxy=example.com:port
-# export https_proxy=example.com:port
+# export http_proxy=http://example.com:port
+# export https_proxy=https://example.com:port
 
 ## set git http and https proxy here for using git http/https protocol
-# git config --local http.proxy example.com:port
-# git config --local https.proxy example.com:port
+# git config --local http.proxy http://example.com:port
+# git config --local https.proxy https://example.com:port
 
 ## set git gitProxy here for using git's git protocol
 # git config --local core.gitProxy /path/to/bin/git-proxy.sh
